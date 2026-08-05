e5aa7e57cec7317793e0c488d3f7df042ca640de
Brzezina <artur.brzezina@capgemini.com>
2026-08-05 15:13:50 +0200

    feat: user creation, login and signup components, create server action, add db connect

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Button, Link, Stack, TextField } from "@mui/material";
+import createUser from "./create-user";
+import { useActionState } from "react";
+
+export default function Login() {
+  const [state, formAction] = useActionState(createUser, { error: "" });
+
+  return (
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          type="email"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          helperText={state.error}
+          error={!!state.error}
+        />
+        <Button type="submit" variant="contained">
+          Signup
+        </Button>
+        <Link href="/auth/login" className="self-center">
+          Login
+        </Link>
+      </Stack>
+    </form>
+  );
+}
