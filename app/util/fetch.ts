f2fd9ca09daa8bfac5d11ba38144087ba7f11310
Brzezina <artur.brzezina@capgemini.com>
2026-08-07 15:06:22 +0200

    feat: add middleware, add unauthorized routes list

--- app/util/fetch.ts
+++ app/util/fetch.ts
@@ -1,11 +1,17 @@
+import { cookies } from "next/headers";
 import { API_URL } from "../constants/api";
 import { getErrorMessage } from "./errors";
+
+const getHeaders = async () => ({
+  Cookie: (await cookies()).toString(),
+});
 
 export const post = async (path: string, formData: FormData) => {
   const res = await fetch(`${API_URL}/${path}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      ...(await getHeaders()),
     },
     body: JSON.stringify(Object.fromEntries(formData)),
   });
@@ -16,4 +22,12 @@ export const post = async (path: string, formData: FormData) => {
   }
 
   return { error: "" };
+};
+
+export const get = async (path: string) => {
+  const res = await fetch(`${API_URL}/${path}`, {
+    headers: { ...(await getHeaders()) },
+  });
+
+  return res.json();
 };
