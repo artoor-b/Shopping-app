4863979cc918dcd2a01d11de145d5614db130354
Brzezina <artur.brzezina@capgemini.com>
2026-08-12 15:34:29 +0200

    feat: upload product image

--- app/products/actions/create-product.ts
+++ app/products/actions/create-product.ts
@@ -1,11 +1,28 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { post } from "../../common/util/fetch";
+import { getHeaders, post } from "../../common/util/fetch";
+import { API_URL } from "@/app/common/constants/api";
 
 export default async function createProduct(formData: FormData) {
   const response = await post("products", formData);
+  const productImage = formData.get("image");
+
+  if (productImage instanceof File && !response.error) {
+    await uploadProductImage(response.data.id, productImage);
+  }
   revalidatePath("/");
 
   return response;
+}
+
+async function uploadProductImage(productId: number, file: File) {
+  const formData = new FormData();
+  formData.append("image", file);
+
+  await fetch(`${API_URL}/products/${productId}/image`, {
+    body: formData,
+    method: "POST",
+    headers: await getHeaders(),
+  });
 }
--- app/products/create-product/create-product-modal.tsx
+++ app/products/create-product/create-product-modal.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { Box, Button, Modal, Stack, TextField } from "@mui/material";
-import { useState } from "react";
+import {
+  Box,
+  Button,
+  Modal,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { CSSProperties, useState } from "react";
 import { FormResponse } from "../../common/interfaces/form-response.interface.ts.js";
 import createProduct from "../actions/create-product";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const styles = {
   position: "absolute",
@@ -17,6 +25,18 @@ const styles = {
   p: 4,
 };
 
+const fileInputStyles: CSSProperties = {
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+};
+
 interface CreateProductModalProps {
   open: boolean;
   handleClose: () => void;
@@ -27,10 +47,12 @@ export default function CreateProductModal({
   handleClose,
 }: CreateProductModalProps) {
   const [response, setResponse] = useState<FormResponse>();
+  const [filename, setFileName] = useState<string>("");
 
   const onClose = () => {
     setResponse(undefined);
     handleClose();
+    setFileName("");
   };
 
   return (
@@ -72,6 +94,23 @@ export default function CreateProductModal({
               helperText={response?.error}
               error={!!response?.error}
             />
+            <Button
+              component="label"
+              variant="outlined"
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload File
+              <input
+                type="file"
+                name="image"
+                style={fileInputStyles}
+                onChange={(e) =>
+                  e.target.files && setFileName(e.target.files[0].name)
+                }
+              />
+            </Button>
+            <Typography>{filename}</Typography>
+
             <Button type="submit" variant="contained">
               Submit
             </Button>
